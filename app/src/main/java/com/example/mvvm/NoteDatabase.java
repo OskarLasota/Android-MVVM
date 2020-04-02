e663aa8f4c3151b3f97e64896ab5864114f6a0c4
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-04-02 16:57:20 +0100

    mvvm architecture

---
+++ app/src/main/java/com/example/mvvm/NoteDatabase.java
@@ -0,0 +1,33 @@
+package com.example.mvvm;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+//singleton
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance;
+
+    public abstract NoteDao noteDao();
+
+    //synchronized means that only 1 thread can access, to prevent multiple being created
+    public static synchronized NoteDatabase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(), NoteDatabase.class, "note_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+
+
+
+
+}
