c8021363455b131c35d6b708b566bd7329a04c5f
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-04-05 18:21:17 +0100

    making some ui tests

--- app/src/androidTest/java/com/example/mvvm/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mvvm/ExampleInstrumentedTest.java
@@ -2,11 +2,31 @@ package com.example.mvvm;
 
 import android.content.Context;
 import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Map;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.swipeLeft;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.*;
 
 /**
@@ -16,6 +36,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -23,4 +47,29 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.mvvm", appContext.getPackageName());
     }
+
+    @Test
+    public void clickAddNoteButton() {
+        onView(withId(R.id.button_add_note))
+                .perform(click());
+    }
+    @Test
+    public void addNote() {
+        String description = "description";
+        String title = "testing";
+        onView(withId(R.id.button_add_note))
+                .perform(click());
+        onView(withId(R.id.edit_text_title)).
+                perform(typeText(title));
+        onView(withId(R.id.edit_text_description)).
+                perform(typeText(description));
+        onView(withId(R.id.number_picker_priority)).
+                perform(swipeUp());
+        onView(withId(R.id.save_note)).
+                perform(click());
+
+    }
+
+
+
 }
